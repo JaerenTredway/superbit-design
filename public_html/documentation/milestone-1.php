71c023b9ab4581bc4a10613ebc0b6093ab5d5bce
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-01-15 19:19:29 -0700

    Started Persona section.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>SuperBit Design</title>
+  </head>
+  
+  <body>
+    <section class="overview">
+      <h1>Purpose</h1>
+      <p>This website will provide customers who are looking for a web development company with a clear and concise understanding of what services and deliverable products SuperBit Design provides.</p>
+      
+      <h1>Audience</h1>
+      <p>The people accessing this site will be prospective clients who want information that will allow them to make a decision about pursuing business dealings with SuperBit Design. Customers will be small business owners who want a new website or who need an existing site repaired or improved upon with such things as online sales functionality, client databases, sheduling systems and so on. </p>
+      
+      <h1>Goal</h1>
+      <p>The end goal of this website is to impress the viewer with the cutting-edge, modern look and feel of the site, outstanding credentials and references, a portfolio with exceptional examples of work done by Will, and overall a smooth and seamless user experience. Ultimately the viewer will pursue a business relationship with SuperBit Design and purchase services.</p>
+    </section>
+    
+    <section class="persona">
+
+
+
+		 <!--Your persona should feature the following at minimum:
+
+		 Name
+		 A detailed description that includes demographic information such as: age, job or role, relevant technology being used, behaviors and attitudes, and the situation or scenario in which they will use your site.
+		 Goal(s) regarding the use of your site.
+		 Additional information regarding your persona may also include:
+
+		 Frustrations your persona may be facing.
+		 Additional information your persona may need in order to achieve their goal. -->
+    </section>
+  </body>
+</html>
+
+
+
+
+
+
+
