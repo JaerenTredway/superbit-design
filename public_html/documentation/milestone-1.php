95f0b26da7529cef09924983cfbaf6c09a491c31
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-01-15 19:54:49 -0700

    Added comments to the Persona section to serve as notes while constructing the Persona.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -16,23 +16,41 @@
 			<p>The people accessing this site will be prospective clients who want information that will allow them to make a decision about pursuing business dealings with SuperBit Design. Customers will be small business owners who want a new website or who need an existing site repaired or improved upon with such things as online sales functionality, client databases, sheduling systems and so on. </p>
 
 			<h2>Goal</h2>
-			<p>The end goal of this website is to impress the viewer with the cutting-edge, modern look and feel of the site, outstanding credentials and references, a portfolio with exceptional examples of work done by Will, and overall a smooth and seamless user experience. Ultimately the viewer will pursue a business relationship with SuperBit Design and purchase services.</p>
+			<p>The end goal of this website is to impress the viewer with the cutting-edge, modern look and feel of the site, outstanding credentials and references, a portfolio with exceptional examples of work done by Will, and overall a smooth and seamless user experience. Ultimately the viewer will be impressed by the site and will choose to pursue a business relationship with SuperBit Design and purchase services.</p>
 	 	</section>
     
     	<section class="persona">
 
 
 
-		 <!--Your persona should feature the following at minimum:
+			<!--Your persona should feature the following at minimum:
+		Name
+		A detailed description that includes demographic information such as: age, job or role, relevant technology being used, behaviors and attitudes, and the situation or scenario in which they will use your site.
+		Goal(s) regarding the use of your site.
+		Additional information regarding your persona may also include:
 
-		 Name
-		 A detailed description that includes demographic information such as: age, job or role, relevant technology being used, behaviors and attitudes, and the situation or scenario in which they will use your site.
-		 Goal(s) regarding the use of your site.
-		 Additional information regarding your persona may also include:
+		Frustrations your persona may be facing.
+		Additional information your persona may need in order to achieve their goal.
 
-		 Frustrations your persona may be facing.
-		 Additional information your persona may need in order to achieve their goal. -->
-    	</section>
+		 	Pictures are particularly useful as they help people to visualise the persona, to make it seem real. They also help designers/developers empathise with the personas more easily.
+			A description gives a brief summary of the persona, which often contains demographic information (sometimes going into surprisingly large amounts of detail). The description might include (among other information) their job, their technology, any pressures they're under and the situation they're in when they use the system/site.
+			Needs, goals & features can either be a straight list for each persona, or a table comparing each persona against each possible need/feature/goal. This can help define a priority list for features (or the needs from which to develop features) before development starts.
+
+			A 'quote' from the persona
+This should summarise their needs/goals into a single sentence. This can be a double edged sword, as this single sentence runs the risk of oversimplifying the personas issues. Quotes do have the added advantage of giving the personas further personality.
+Frustrations
+These are issues that the persona encounters, either with the existing system or because there's no existing system. These can clarify to designers/developers exactly what the biggest issues this persona faces.
+Ideal features
+This is very similar to features, but with no restrictions placed on their feasibility... these may be impossible to produce, but sometimes help designers/developers think of alternative approaches that are more realistic.
+'Need to know'
+Any information the persona needs when trying to do their goal. These help designers/developers understand the key information needed to communicate to users.
+Behaviours
+These are the typical behaviours the persona exhibits. They're usually only shown if the personas different behaviours lead to different needs/requirements of the system in development.
+Scenarios
+These are sample use cases the persona may do. They can be based on either their goals or their use of an existing system. This can clarify exactly how a system will be/is used.
+
+			-->
+		</section>
   	</body>
 </html>
 
