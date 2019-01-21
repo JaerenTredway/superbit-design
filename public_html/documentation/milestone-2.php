5afa9c3ee1eacebf80c9d3d27cfce455abc560d4
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-01-21 10:41:29 -0700

    Added html for milestone 2a.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <title>SuperBit Design</title>
+        <link href="./documentation-styles.css" rel="stylesheet" type="text/css"/>
+		<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet"/>
     </head>
     
     <body>
@@ -13,9 +15,71 @@
             </ul>
         </nav>
         
-        
         <section>
+            <h1>PWP Milestone 2α</h1>
+            <h2>Content Strategy</h2>
+            <p>The SuperBit Design website will have the following pages:</p>
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>Portfolio</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
             
+            <h3>Home</h3>
+            <ol>
+                <li>fixed header with logo, title, hamburger dropdown nav menu</li>
+                <li>hero/ jumbotron with image and quote</li>
+                <li>gallery of example services/ products</li>
+                <li>footer with copyright, logo link to homepage</li>
+            </ol>
+            
+            
+            <h3>Services</h3>
+            <ol>
+                <li>fixed header with logo, title, hamburger dropdown nav menu</li>
+                <li>detailed list of services and prices</li>
+                <li>one testimonial</li>
+                <li>footer with copyright, logo link to homepage</li>
+            </ol>
+            
+            <h3>Portfolio</h3>
+            <ol>
+                <li>fixed header with logo, title, hamburger dropdown nav menu</li>
+                <li>gallery of my work</li>
+                <li>one testimonial</li>
+                <li>footer with copyright, logo link to homepage</li>
+            </ol>
+            
+            <h3>About</h3>
+            <ol>
+                <li>fixed header with logo, title, hamburger dropdown nav menu</li>
+                <li>brief bio</li>
+                <li>one testimonial</li>
+                <li>footer with copyright, logo link to homepage</li>
+            </ol>
+            
+            <h3>Contact</h3>
+            <ol>
+                <li>fixed header with logo, title, hamburger dropdown nav menu</li>
+                <li>contact information</li>
+                <li>footer with copyright, logo link to homepage</li>
+            </ol>
+            
+            <h1> Mobile WireFrames</h1>
+            <img src="" alt="homepage">
+            <img src="" alt="services">
+            <img src="" alt="portfolio">
+            <img src="" alt="about">
+            <img src="" alt="contact">
+            
+            <h1> Desktop WireFrames</h1>
+            <img src="" alt="homepage">
+            <img src="" alt="services">
+            <img src="" alt="portfolio">
+            <img src="" alt="about">
+            <img src="" alt="contact">
         </section>
     </body>
 </html>
