17fc9fd865ed8753df0cff097034f0a4bed3843e
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-01-21 20:57:52 -0700

    Added width to images.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -68,8 +68,11 @@
             </ol>
             
             <h1> Mobile WireFrames</h1>
-            <img class="mobile" src="../resources/home.png" alt="homepage">
-            <img class="mobile" src="../resources/services.png" alt="services">
+            <div class="mobile">
+                <img src="../resources/home.png" alt="homepage">
+            </div>
+            
+            <img src="../resources/services.png" alt="services" class="mobile">
             <img class="mobile" src="../resources/portfolio.png" alt="portfolio">
             <img class="mobile" src="../resources/about.png" alt="about">
             <img class="mobile" src="../resources/contact.png" alt="contact">
